b3e417e1558c3e3da36e9af04b42c8661944c8e6
jfalencastro <jalencastro@segura.security>
2026-02-09 18:05:28 -0300

    Update __init__.py

--- akv_dsm_sync/__init__.py
+++ akv_dsm_sync/__init__.py
@@ -1,6 +1,8 @@
 import azure.functions as func
 import logging
+import json
+import requests
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("CHEGUEI NO MAIN")
+    logging.info("Imports OK")
     return func.HttpResponse("OK", status_code=200)
