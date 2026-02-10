9768b6dfb8013333079f39daa5e46d6785ea66b7
jfalencastro <jalencastro@segura.security>
2026-02-10 16:40:26 -0300

    Update __init__.py

--- akv_dsm_sync/__init__.py
+++ akv_dsm_sync/__init__.py
@@ -1,7 +1,7 @@
 import logging
 import azure.functions as func
+import requests
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Sem libs externas")
+    logging.info("Requests version loaded")
     return func.HttpResponse("OK", status_code=200)
-
