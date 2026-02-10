8e3478064d673f25ad86fef63fd198962b4b3649
jfalencastro <jalencastro@segura.security>
2026-02-10 16:12:21 -0300

    Update __init__.py

--- akv_dsm_sync/__init__.py
+++ akv_dsm_sync/__init__.py
@@ -1,10 +1,16 @@
 import logging
 import azure.functions as func
+from azure.identity import DefaultAzureCredential
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info(">>> INICIO DA FUNCAO <<<")
+    logging.info("INICIO - Teste Managed Identity")
 
-    return func.HttpResponse(
-        "Function V1 funcionando",
-        status_code=200
-    )
+    try:
+        credential = DefaultAzureCredential()
+        token = credential.get_token("https://management.azure.com/.default")
+        logging.info("Managed Identity OK")
+    except Exception as e:
+        logging.error(f"Erro Managed Identity: {e}")
+        return func.HttpResponse("MI falhou", status_code=500)
+
+    return func.HttpResponse("Managed Identity funcionando", status_code=200)
