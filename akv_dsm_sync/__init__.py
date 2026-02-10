e9cc54ba65f72df69cd3e2ef25ee660ea3c6b514
jfalencastro <jalencastro@segura.security>
2026-02-10 16:37:14 -0300

    Update __init__.py

--- akv_dsm_sync/__init__.py
+++ akv_dsm_sync/__init__.py
@@ -1,14 +1,7 @@
 import logging
 import azure.functions as func
-from azure.identity import ManagedIdentityCredential
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Teste MI iniciado")
+    logging.info("Sem libs externas")
+    return func.HttpResponse("OK", status_code=200)
 
-    credential = ManagedIdentityCredential()
-
-    token = credential.get_token("https://management.azure.com/.default")
-
-    logging.info("Token Azure obtido")
-
-    return func.HttpResponse("MI OK", status_code=200)
